b98af5f7330a496148b5d52db62eda143d628d0a
ThierryCols <thierrycolsenet@gmail.com>
2017-10-28 18:20:31 +0200

    :rocket: change case and create keybindings

--- extension.js
+++ extension.js
@@ -6,21 +6,89 @@ const vscode = require('vscode');
 // your extension is activated the very first time the command is executed
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "fast-case-change" is now active!');
+    let pascalCmd = vscode.commands.registerCommand('extension.fastCaseChange.pascal', function () {
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        var selection = editor.selection;
+        var text = editor.document.getText(selection);
+        editor.edit(builder => {
+            builder.replace(selection, toPascalCase(text))
+        })
     });
+    context.subscriptions.push(pascalCmd);
 
-    context.subscriptions.push(disposable);
+    let camelCmd = vscode.commands.registerCommand('extension.fastCaseChange.camel', function () {
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        var selection = editor.selection;
+        var text = editor.document.getText(selection);
+        editor.edit(builder => {
+            builder.replace(selection, toCamelCase(text))
+        })
+    });
+    context.subscriptions.push(camelCmd);
+
+    let snakeCmd = vscode.commands.registerCommand('extension.fastCaseChange.snake', function () {
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        var selection = editor.selection;
+        var text = editor.document.getText(selection);
+        editor.edit(builder => {
+            builder.replace(selection, toSnakeCase(text))
+        })
+    });
+    context.subscriptions.push(snakeCmd);
+
+    let kebapCmd = vscode.commands.registerCommand('extension.fastCaseChange.kebap', function () {
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        var selection = editor.selection;
+        var text = editor.document.getText(selection);
+        editor.edit(builder => {
+            builder.replace(selection, toKebapCase(text))
+        })
+    });
+    context.subscriptions.push(kebapCmd);
+
+    let lowerCmd = vscode.commands.registerCommand('extension.fastCaseChange.lower', function () {
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        var selection = editor.selection;
+        var text = editor.document.getText(selection);
+        editor.edit(builder => {
+            builder.replace(selection, text.toLowerCase())
+        })
+    });
+    context.subscriptions.push(lowerCmd);
+
+    let upperCmd = vscode.commands.registerCommand('extension.fastCaseChange.upper', function () {
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+
+        var selection = editor.selection;
+        var text = editor.document.getText(selection);
+        editor.edit(builder => {
+            builder.replace(selection, text.toUpperCase())
+        })
+    });
+    context.subscriptions.push(upperCmd);
 }
 exports.activate = activate;
 
@@ -28,3 +96,29 @@ exports.activate = activate;
 function deactivate() {
 }
 exports.deactivate = deactivate;
+
+function getWords(targetString) {
+    return targetString.split(/(?:-|_)|(?=[A-Z])/).map(word => word.toLowerCase());
+}
+
+function toPascalCase(targetString) {
+    if (!targetString)
+        return targetString;
+    return getWords(targetString).reduce((pascalStr, word) => {
+        return pascalStr.concat(word[0].toUpperCase().concat(word.slice(1)));
+    }, '');
+}
+
+function toCamelCase(targetString) {
+    return getWords(targetString).reduce((camelString, word) => {
+        return camelString.concat(word[0].toUpperCase().concat(word.slice(1)));
+    });
+}
+
+function toSnakeCase(targetString) {
+    return getWords(targetString).join('_');
+}
+
+function toKebapCase(targetString) {
+    return getWords(targetString).join('-');
+}
